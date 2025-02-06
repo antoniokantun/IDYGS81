99d72e75cb407d173507ebadf1bed444c92f49e7
antoniokantun <22393267@utcancun.edu.mx>
2025-02-05 19:00:23 -0500

    Add project files.

---
+++ IDYGS81/Menu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDYGS81
+{
+    public class Menu
+    {
+        public decimal Suma(decimal num1, decimal num2)
+        {
+            decimal resultado = num1 + num2;
+            return resultado;
+        }
+
+        public decimal resta(decimal num1, decimal num2)
+        {
+            decimal resultado = num1 - num2;
+            return resultado;
+        }
+
+        public decimal multiplicacion(decimal num1, decimal num2)
+        {
+            decimal resultado = num1 * num2;
+            return resultado;
+        }
+
+        public decimal division(decimal num1, decimal num2)
+        {
+            decimal resultado = num1 / num2;
+            return resultado;
+        }
+    }
+}
---
+++ IDYGS81/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using IDYGS81;
+
+Menu menu = new Menu();
+
+Persona persona = new Persona();
+
+Console.WriteLine("Ingrese su nombre");
+persona.Nombre = Console.ReadLine();
+
+Console.WriteLine("Ingrese su apellido");
+persona.Apellido = Console.ReadLine();
+
+    Console.WriteLine("1. Suma");
+    Console.WriteLine("2. Resta");
+    Console.WriteLine("3. Multiplicacion");
+    Console.WriteLine("4. Division");
+    Console.WriteLine("5. Salir");
+
+Console.WriteLine("Ingrese una opcion");
+int opcion = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ingrese el primer numero");
+decimal num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ingrese el segundo numero");
+decimal num2 = Convert.ToInt32(Console.ReadLine());
+
+switch (opcion)
+{
+    case 1:
+        Console.WriteLine($"La {persona.Nombre} {persona.Apellido} la suma es: {menu.Suma(num1, num2)}");
+        break;
+    case 2:
+        Console.WriteLine($"La {persona.Nombre} {persona.Apellido} la resta es: {menu.resta(num1, num2)}");
+        break;
+    case 3:
+        Console.WriteLine($"La {persona.Nombre} {persona.Apellido} la multiplicacion es: {menu.multiplicacion(num1, num2)}");
+        break;
+    case 4:
+        Console.WriteLine($"La {persona.Nombre} {persona.Apellido} la division es: {menu.division(num1, num2)}");
+        break;
+    default:
+        Console.WriteLine("Opcion no valida");
+        break;
+}
+
+
+
+
+
+
